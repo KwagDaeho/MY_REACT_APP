57d40ab80d0219fca62125d65c8620ddd9ef49a3
할일하는대호 <Haraho@Haraho-MacBook-Pro.local>
2021-07-30 21:43:55 +0900

    docs:TS-REACT-APP

---
+++ src/daeho.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var num = 1;
+num = 2;
+function Daeho() { }
+exports.default = Daeho;
---
+++ src/daeho.ts
@@ -0,0 +1,14 @@
+import React from "react";
+
+var num = 1;
+num = 2;
+
+class Kwag {
+  constructor() {
+    console.log("It is TSC.");
+  }
+}
+new Kwag();
+function Daeho() {}
+
+export default Daeho;
