b26a49463b1c06b0ef40268512229978287b8c04
Haraho <1438eogh@gmail.com>
2021-08-02 21:28:49 +0900

    refactor:use state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React, { Component } from "react";
 import "./App.css";
 import Content from "./components/Content.js";
 import Subject from "./components/Subject.js";
+import ETC from "./components/ETC";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "REACT", sub: "Hello. This is REACT APP!!" },
+      contents: [
+        { id: 1, title: "HTML_1", desc: "this is description part1..." },
+        { id: 2, title: "HTML_2", desc: "this is description part2..." },
+        { id: 3, title: "HTML_3", desc: "this is description part3..." },
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="REACT" sub="Hello. This is REACT APP!!" />
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        />
+        <ETC data={this.state.contents} />
         <Content title="HTML" desc="this is description." />
       </div>
     );
---
+++ src/components/ETC.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class ETC extends Component {
+  render() {
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while (i < data.length) {
+      lists.push(
+        <p key={data[i].id}>
+          {data[i].id}(이)랑 {data[i].title} (이)라는 값이 나왔당께롱.{" "}
+          {data[i].desc} 요것도 같이 나왔당께롱.
+        </p>
+      );
+      i = i + 1;
+    }
+
+    return <div>{lists}</div>;
+  }
+}
+
+export default ETC;
